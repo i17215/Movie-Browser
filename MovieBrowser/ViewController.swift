89903b61668862618737d5c244d950a68a37206a
Kirill Koleno <k.koleno@icloud.com>
2018-11-18 18:51:58 +0400

    Initial Commit

---
+++ MovieBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieBrowser
+//
+//  Created by Kirill Koleno on 18/11/2018.
+//  Copyright © 2018 Kirill Koleno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
